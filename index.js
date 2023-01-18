48b1d58fb837238d08b8a0aa1c054fd473638caf
Man Patel <man.secretmindtech@gmail.com>
2023-01-18 17:01:48 +0530

    check auto deploy

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
+  console.log("Just got a request!")
+  res.send('Yoooooooooooooooooooooo!')
 })
 app.listen(process.env.PORT || 3000)
